f6d96d5d2c7f646c139bd86728ee50951ff3348f
kateincoding <kateincoding@gmail.com>
2021-05-30 01:16:11 -0500

    first script for test different id

---
+++ level_0/level_0.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import requests
+
+success_votes = 0
+user_id = int(input("Please write your ID: "))
+number_print = 1024
+votation = {'id': user_id, 'holdthedoor': 'Submit'}
+
+for i in range(0, number_print):
+    r = requests.post('http://158.69.76.135/level0.php', data=votation)
+    if r.status_code == 200:
+        success_votes +=1
+
+print("print success: {}".format(success_votes))
