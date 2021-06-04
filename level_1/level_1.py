11eb83a7d84bbe236058ba66b7a617acb2e7a4b6
kateincoding <kateincoding@gmail.com>
2021-06-03 21:00:15 -0500

    enter your votes with an ID

---
+++ level_1/level_1.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+from bs4 import BeautifulSoup
+import requests
+import sys
+
+# r = requests.post('http://158.69.76.135/level1.php', data=votation)
+
+success_votes = 0
+error_cases = 0
+user_id = int(input("Please write your ID: "))
+number_print = 4096
+url = "http://158.69.76.135/level1.php"
+
+for i in range(0, number_print):
+    cookies_page = requests.session()
+    r = cookies_page.get(url)
+#    print(r.cookies)
+
+    soup = BeautifulSoup(r.text, "lxml")
+    key_value = soup.find('form').find('input', {'name' : 'key'})['value']
+
+    votation = {'id': user_id, 'holdthedoor': 'Submit', 'key': key_value}
+    vote = cookies_page.post(url, data=votation)
+#    print(vote.cookies)
+
+    if vote.status_code == 200:
+        success_votes +=1
+    else:
+        error_cases += 1
+
+print("-------------------")
+print("print success: {}".format(success_votes))
+print("error_cases: {}".format(error_cases), file=sys.stderr)
+print("-------------------")
+if error_cases == 0:
+    print("Success operation: 100% of {} votes".format(success_votes))
